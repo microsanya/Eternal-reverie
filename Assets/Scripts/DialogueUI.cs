86dfe012f28613b3e2d71267df6524564a0d667c
microsanya <159100376+microsanya@users.noreply.github.com>
2026-01-21 20:18:02 +0700

    Machine typing text

--- Assets/Scripts/DialogueUI.cs
+++ Assets/Scripts/DialogueUI.cs
@@ -1,29 +1,111 @@
 using UnityEngine;
 using TMPro;
+using System.Collections;
 
 public class DialogueUI : MonoBehaviour
 {
     public static DialogueUI Instance;
 
     public TextMeshProUGUI dialogueText;
-    public float dusplayTime = 3f;
+    public float letterDelay = 0.04f;
+
+    private Coroutine typingCoroutine;
+    private string currentText;
+    private bool isTyping;
+    private bool isOpen;
+
+    //public float dusplayTime = 3f;
 
     private void Awake()
     {
-        Instance = this;
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        // Instance = this;
         dialogueText.text = "";
+        dialogueText.gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (!isOpen)
+        {
+            return;
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (isTyping)
+            {
+                SkipTyping();
+            }
+            else
+            {
+                Close();
+            }
+        }
     }
 
     public void ShowText(string text)
     {
-        StopAllCoroutines();
-        StartCoroutine(ShowRoutine(text));
+        if (isOpen)
+        {
+            Close();
+        }
+        
+        currentText = text;
+        isOpen = true;
+        dialogueText.gameObject.SetActive(true);
+        typingCoroutine = StartCoroutine(TypeText());
+        //StopAllCoroutines();
+        //StartCoroutine(ShowRoutine(text));
     }
 
-    private System.Collections.IEnumerator ShowRoutine(string text)
+    IEnumerator TypeText()
     {
-        dialogueText.text = text;
-        yield return new WaitForSeconds(dusplayTime);
+        isTyping = true;
         dialogueText.text = "";
+
+        foreach (char letter in currentText)
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(letterDelay);
+        }
+
+        isTyping = false;
     }
+
+    void SkipTyping()
+    {
+        //if (typingCoroutine != null)
+        //{
+            StopCoroutine(typingCoroutine);
+        //}
+
+        dialogueText.text = currentText;
+        isTyping = false;
+    }
+
+    void Close()
+    {
+        if (typingCoroutine != null)
+        {
+            StopCoroutine(typingCoroutine);
+        }
+        dialogueText.text = "";
+        dialogueText.gameObject.SetActive(false);
+        isTyping = false;
+        isOpen = false;
+    }
+    //private System.Collections.IEnumerator ShowRoutine(string text)
+    //{
+    //    dialogueText.text = text;
+    //    //yield return new WaitForSeconds(dusplayTime);
+    //    dialogueText.text = "";
+    //}
 }
